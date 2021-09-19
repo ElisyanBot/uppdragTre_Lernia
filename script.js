3bfb5019fd4e674360f9e5ec47b50a8838481371
Marcus Larsson <77403295+ElisyanBot@users.noreply.github.com>
2021-09-19 18:42:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const exitBtn = document.querySelector('#ExitBtn');
 
 exitBtn.addEventListener('click', () => {
-	changeText('welcomeText', 'textColorBlue', 'Goodbye world!');
+	changeText('WelcomeText', 'textColorBlue', 'Goodbye world!');
 })
 
-function changeText (htmlElement, addClass, newInnerText){	
-	const selectedElement = document.querySelector(`.${htmlElement}`);
+function changeText (htmlElementID, addClass, newInnerText){	
+	const selectedElement = document.querySelector(`#${htmlElementID}`);
 		addClass !== null || undefined ? 
 			selectedElement.classList.add(`${addClass}`) : //if
 			selectedElement.classList.remove(); //else
